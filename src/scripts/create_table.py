f2a600b9e93c44f8ad3a89769e56e0a7f8ef74a7
oguzcan celik <oguzcancelk@gmail.com>
2019-07-25 23:10:27 +0300

    add scripts folder

---
+++ src/scripts/create_table.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+
+def create_table(table_name, columns):
+    with connection:
+        c.execute("CREATE TABLE " + table_name + columns)
+
+
+connection = sqlite3.connect('../db.sqlite3')
+c = connection.cursor()
+
+create_table('playlist_movies', """ (playlist_id text, playlist_name text, playlist_url_name text, 
+percentage int, release_year int, movie_id text, movie_name text, movie_url text)""")
